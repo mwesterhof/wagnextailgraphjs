fa7367374b69aacbad5dbee56105d62df73cba5a
Marco Westerhof <m.westerhof@lukkien.com>
2022-05-06 00:53:13 +0200

    alt text

--- client/components/blocks/HeroImageBlock.js
+++ client/components/blocks/HeroImageBlock.js
@@ -4,8 +4,6 @@ import classes from './HeroImageBlock.module.sass'
 
 
 const HeroImageBlock = ({image}) => {
-    console.log(image)
-
     return (
         <div className={classes.HeroImageBlock}>
             <h3>heroimageblock</h3>
@@ -13,6 +11,7 @@ const HeroImageBlock = ({image}) => {
                 src={image.renditions[0].url}
                 width={300}
                 height={300}
+                alt={image.title}
             />
         </div>
     )
